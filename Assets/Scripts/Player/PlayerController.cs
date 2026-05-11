3666a824490f0c2c5c9233a41151186c4f37f965
lugasparihukamasandika-crypto <lugasparihukamasandika@gmail.com>
2026-05-11 14:28:35 +0700

    fix: connect player to scriptable object

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,22 +3,35 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    public float currentHP = 100;
-    public float speed = 5f;
+    [Header("Player Data")]
+    public PlayerData playerData;
+
+    private float currentHP;
+    private float speed;
+
     private PlayerInput playerInput;
     private Vector2 moveInput;
 
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
+
+        // Ambil data dari Scriptable Object
+        currentHP = playerData.maxHP;
+        speed = playerData.moveSpeed;
     }
-    
-    
+
     void Update()
     {
-        if (playerInput == null) return;
-        
+        // Player hanya bisa bergerak saat Playing
+        if (GameManager.Instance.currentState != GameState.Playing)
+            return;
+
+        if (playerInput == null)
+            return;
+
         moveInput = playerInput.actions["Move"].ReadValue<Vector2>();
+
         float h = moveInput.x;
         float v = moveInput.y;
 
@@ -36,10 +49,12 @@ public class PlayerController : MonoBehaviour
     void TakeDamage(float dmg)
     {
         currentHP -= dmg;
-        Debug.Log("Player HP: " + currentHP);
+
+        Debug.Log("Player HP : " + currentHP);
 
         if (currentHP <= 0)
         {
+            currentHP = 0;
             GameManager.Instance.GameOver();
         }
     }
